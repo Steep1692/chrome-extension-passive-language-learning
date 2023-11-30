61dd2eb96cebe1d8b5e3c00e9ddee1ab4ac34c39
dmlen <dmlen@rollncode.com>
2023-11-30 19:02:42 +0200

    [CEPLL] Implement words parsing utils

--- src/words-base/parse-words-to-state-data.js
+++ src/words-base/parse-words-to-state-data.js
@@ -19,13 +19,40 @@ const __dirname = new URL('.', import.meta.url).pathname
 const inDirPath = path.join(__dirname, './raw')
 const outDirPath = path.join(__dirname, './jsons')
 
+const UK_FOLDER_ID_TO_NAME = {
+  "100-adverbs": "100 Прислівників",
+  "100-conjunction-words": "100 Сполучників",
+  "100-interjection-words": "100 Міжслів",
+  "100-nouns": "100 Іменників",
+  "100-preposition-words": "100 Прийменників",
+  "100-verbs": "100 Дієслів",
+  "countable": "Зліченні",
+  "linking-verbs": "Зв'язуючі дієслова",
+  "object-pronouns": "Займенники-об'єкти",
+  "personal-pronouns": "Особисті займенники",
+  "possesive-pronouns": "Присвійні займенники",
+  "possesive-words": "Присвійні слова",
+  "question-complex-words": "Складні питальні слова",
+  "question-words": "Питальні слова",
+  "reflexive-pronouns": "Займенники-вказівники",
+  "uncountable": "Незліченні"
+}
+
+const folderIdToName = (folderId, langTo) => {
+  if (langTo === LANG.uk) {
+    return UK_FOLDER_ID_TO_NAME[folderId]
+  }
+
+  return folderId
+}
+
 const getLangsTo = (langFrom) => {
   const langsTo = LANGS.filter((lang) => lang !== langFrom)
   return langsTo
 }
 
 const parseText = (text, langFrom, langTo) => {
-  const translator = new Translator(fetch, langFrom, langTo)
+  const translator = new Translator(langFrom, langTo, fetch)
 
   const parseExecutor = (resolve, reject) => {
     const promises = []
@@ -117,31 +144,41 @@ async function walkAndConvertFiles(currentPath, relativePath, langFrom) {
   }
 }
 
-const generateFoldersJson = (lang) => {
-  const inFilesPath = path.join(inDirPath, lang)
-  if (!fs.existsSync(inFilesPath)) {
-    return
-  }
+const generateFoldersJson = (langFrom) => {
+  const langsTo = getLangsTo(langFrom)
 
-  const jsonPath = path.join(outDirPath, lang, '__folders.json')
-  if (fs.existsSync(jsonPath)) {
-    console.log('SKIP folders.json for ' + lang)
-    return
-  }
-
-  const folderIds = fs.readdirSync(inFilesPath)
-
-  const folders = folderIds.map((folderId) => {
-    const id = folderId.replace(FORMAT_IN, '')
-    return {
-      id,
-      name: id,
-      entriesId: id,
+  for (const langTo of langsTo) {
+    const inFilesPath = path.join(inDirPath, langFrom)
+    if (!fs.existsSync(inFilesPath)) {
+      return
     }
-  })
 
-  const json = JSON.stringify(folders, null, 2)
-  fs.writeFileSync(jsonPath, json)
+    const outPath = path.join(outDirPath, langFrom, langTo)
+
+    if (!fs.existsSync(outPath)) {
+      fs.mkdirSync(outPath, { recursive: true })
+    }
+
+    const jsonPath = path.join(outPath, '__folders.json')
+    if (fs.existsSync(jsonPath)) {
+      console.log('SKIP folders.json for ' + langFrom)
+      return
+    }
+
+    const folderIds = fs.readdirSync(inFilesPath)
+
+    const folders = folderIds.map((folderId) => {
+      const id = folderId.replace(FORMAT_IN, '')
+      return {
+        id,
+        name: folderIdToName(id, langTo),
+        entriesId: id,
+      }
+    })
+
+    const json = JSON.stringify(folders, null, 2)
+    fs.writeFileSync(jsonPath, json)
+  }
 }
 
 const generateFolderEntriesJson = (langFrom) => {
@@ -177,12 +214,11 @@ const generateFolderEntriesJson = (langFrom) => {
 
 const main = async () => {
   for (const lang of LANGS) {
-    generateFoldersJson(lang)
-
     const inFilesPath = path.join(inDirPath, lang)
     await walkAndConvertFiles(inFilesPath, lang, lang)
 
     generateFolderEntriesJson(lang)
+    generateFoldersJson(lang)
   }
 }
 
